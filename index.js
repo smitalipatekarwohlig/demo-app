e411e73b7a719f3397a4a6951b6dbc7d469e46dd
Smitali Patekar <smitali.patekar@wohlig.com>
2024-05-03 12:03:03 +0530

    changes

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+// Add this to the very top of the first file loaded in your app
+var apm = require("elastic-apm-node").start({
+  serviceName: "my-service-name",
+  secretToken: "IoEQpEKhEPRMriOKlu",
+  serverUrl:
+    "https://a67f4013d9e04cc58c2c3daf58e974ef.apm.us-central1.gcp.cloud.es.io:443",
+  environment: "my-environment",
+});
+console.log("apm", apm);
+
 app.get("/", (req, res) => {
   res.send("Node app is running!");
 });
